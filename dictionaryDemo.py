464d5f37f4e724dccf1d216f13cb9779b366d326
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-25 08:11:27 -0400

    Updated from Brython Server: 4/25/2018 8:11:27 AM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,13 @@
+#Ray Tso
+#4/25/18
+#dictionaryDemo.py
 
+words=['computer','mortify','dog','firetruck','yes','python','cat']
+
+words.sort()
+
+num=int(input('What number word do you want ?:'))
+if num<=0 or num>len(words)+1:
+    print('Invalid number')
+else:
+    print(words[num-1])
