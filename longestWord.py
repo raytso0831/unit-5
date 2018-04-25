534597c6c51f0a56ed184f2e65c38c6169a26f2f
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-25 08:34:45 -0400

    Updated from Brython Server: 4/25/2018 8:34:45 AM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,16 @@
+#Ray Tso
+#4/23/18
+#longestword.py
 
+
+words = input('Enter a list of words: ').split(' ')
+
+
+l = 0
+word = ""
+for w in words:
+    lenn = len(h)
+    if lenn > l:
+        l = lenn
+        word = w
+print("The longest word is", word)
