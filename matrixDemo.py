6b70bc2d8fee1d54f24cda3200cec5eb5a0e665d
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-27 08:36:55 -0400

    Updated from Brython Server: 4/27/2018 8:36:54 AM

--- matrixDemo.py
+++ matrixDemo.py
@@ -2,4 +2,16 @@
 #4/27/18
 #matrixDemo.py
 
+def printBoard(board):
+    for r in range(0,3):
+        for c in range(0,3):
+            print(board[r][c], end=' ')
+        print()
 
+board=[['a','b','c'],['d','e','f'],['g','h','i']]
+printBoard(board)
+
+row=int(input('Enter a row:'))
+col=int(input('Enter a column'))
+board[row-1][col-1]='X'
+printBoard(board)
