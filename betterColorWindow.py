544c25447f022c43736e20252e35f430b9068eb4
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-27 08:43:52 -0400

    Updated from Brython Server: 4/27/2018 8:43:52 AM

--- betterColorWindow.py
+++ betterColorWindow.py
@@ -1,1 +1,20 @@
+#Ray Tso
+#4/27/18
+#betterColorWindow.py
+
+from ggame import*
+def mouseClick(event):
+    
+    red=Color(0xFF0000,1) #this is the color red
+    green=Color(0x00FF00,1)
+    blue=Color(0x0000FF,1)
+    black=Color(0x000000,1)
+    
+    colors = [red, blue, green, black]
+    line = LineStyle(1,colors)
+    square = RectangleAsset(1000,1000,line,colors)
+    Sprite(square)
+    
+    App().listenMouseEvent('click',mouseClick)
+    App().run()
 
