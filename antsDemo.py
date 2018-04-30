71068d2959ae31a25e43b595d16987b4b6dfce00
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-04-30 08:11:15 -0400

    Updated from Brython Server: 4/30/2018 8:11:14 AM

--- antsDemo.py
+++ antsDemo.py
@@ -2,4 +2,18 @@
 #4/30/18
 #antsDemo.py
 
+from ggame import *
+from random import randint
 
+ANTS=10
+WIDTH=1000
+HEIGHT=500
+
+if __name__=='__main__':
+    red=Color(0xFF0000,2)
+    ant=CircleAsset(20,LineStyle(1,red),red)
+    
+    for i in range(ANTS):
+        Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT)))
+    
+    App().run()
