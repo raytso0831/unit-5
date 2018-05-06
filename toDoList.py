8d3c8bc3a4dcf738c8889639e73ed44cab46531d
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-06 18:47:03 -0400

    Update toDoList.py

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,23 @@
+#Ray Tso
+#4/30/18
+#toDoList.py
 
+toDoList = []
+print("Valid commands: add, delete, print, quit")
+
+while True:
+    task = input("> ")
+    if "quit" in task:
+        break
+    elif "print" in task:
+        for w in toDoList:
+            print(w)
+    elif task[:4] == "add ":
+        toDoList.append(task[4:])
+    elif task[7:] == "delete ":
+        if task[7:] in toDoList:
+            toDoList.remove(task[7:])
+        else:
+            print(task[7:], "not in list")
+    else:
+        print("Invalid Command")
