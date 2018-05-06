2b326d765cb4c9cf54ba1a7d17b9647d75ed993c
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-06 17:58:32 -0400

    Update longestWord.py

--- longestWord.py
+++ longestWord.py
@@ -2,14 +2,13 @@
 #4/23/18
 #longestword.py
 
+words = input('Enter some words: ').split(' ')
 
-words = input('Enter a list of words: ').split(' ')
-
-l = 0
-word = ""
-for w in words:
-    lenn = len(w)
-    if lenn > l:
-        l = lenn
-        word = w
-print("The longest word is", word)
+longest = 0
+for item in words:
+    if longest < len(item):
+        longest = len(item)
+        
+for item in words:
+    if len(item) == longest:
+        print(item)
