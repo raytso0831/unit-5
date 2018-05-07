4e58938756a1b0a98c72897285de2e4b24a265e6
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-05-07 08:31:31 -0400

    Updated from Brython Server: 5/7/2018 8:31:31 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,25 @@
+#Ray Tso
+#5/7/18
+#quiz.py
 
+def penultimate(A):
+    return(A[len(A)-1])
+    
+def plusEquals(B,C):
+    for i in range(0,len(B)):
+        B[i] += C
+    return B
+
+
+    
+def smallest(D):
+    D1 = D[0]
+    for i in D:
+        if i<D1:
+            D1 = i
+    return D1
+    
+    
+print(penultimate(['clock','table','computers']))
+print([1,2,3,4],10)
+print(smallest([3,-1,5,-2,7,2,1]))
